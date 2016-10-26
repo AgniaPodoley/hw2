b83d69444c347f36238c34febb700b6c05a79347
AgniaPodoley <Ufyz.sova@yandex.ru>
2016-10-26 16:02:41 +0300

    дз2

--- hw1.php
+++ hw1.php
@@ -3,3 +3,37 @@ for ($i = 1; $i < 100; $i++){
     if($i%2 ==0)
         echo $i."<br />";
 }
+
+
+$winter= array();
+$Mass[] = 'Декабрь';
+$Mass[] = 'Январь';
+$Mass[] = 'Февраль';
+foreach($Mass as $winter)
+{
+    echo $winter . '<br>';
+}
+$spring= array();
+$Mass2[] = 'Март';
+$Mass2[] = 'Апрель';
+$Mass2[] = 'Май';
+foreach($Mass2 as $spring)
+{
+    echo $spring . '<br>';
+}
+$summer= array();
+$Mass3[] = 'Июнь';
+$Mass3[] = 'Июль';
+$Mass3[] = 'Август';
+foreach($Mass3 as $summer)
+{
+    echo $summer . '<br>';
+}
+$autumn= array();
+$Mass4[] = 'Сентябрь';
+$Mass4[] = 'Октябрь';
+$Mass4[] = 'Ноябрь';
+foreach($Mass4 as $autumn)
+{
+    echo $autumn . '<br>';
+}
